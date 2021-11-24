57972c7ca6589ebf24d39878ebf2bde8caa2a113
bhatia-di <bhatia.di@northeastern.edu>
2021-11-24 04:12:25 -0500

    ui set up added

---
+++ zendesk_ticket_viewer_ui/src/App.ts
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/app.scss";
+
+
+const App: React.FC = () => {
+return (
+<div className="wrapper">
+<h1>React 17 and TypeScript 4 App!🚀</h1>
+</div>
+  );
+};
+export default App;
