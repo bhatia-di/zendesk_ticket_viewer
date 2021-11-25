90a122635a44c17452f5714222e070e56c603862
bhatia-di <bhatia.di@northeastern.edu>
2021-11-24 21:08:43 -0500

    ui set up added

--- zendesk_ticket_viewer_ui/src/App.tsx
+++ zendesk_ticket_viewer_ui/src/App.tsx
@@ -4,9 +4,9 @@ import "./styles/app.scss";
 
 const App: React.FC = () => {
 return (
-<div className="wrapper">
-<h1>React 17 and TypeScript 4 App!🚀</h1>
-</div>
+    <div className="wrapper">
+        <h1>React 17 and TypeScript 4 App!🚀</h1>
+    </div>
   );
 };
 export default App;
--- zendesk_ticket_viewer_ui/webpack.config.ts
+++ zendesk_ticket_viewer_ui/webpack.config.ts
@@ -1,60 +1,38 @@
 import path from "path";
-import { Configuration, DefinePlugin } from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import { Configuration } from "webpack";
 
-const webpackConfig = (): Configuration => ({
+const config: Configuration = {
   entry: "./src/index.tsx",
-  ...(process.env.production || !process.env.development
-    ? {}
-    : { devtool: "eval-source-map" }),
-
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
-  },
-  output: {
-    path: path.join(__dirname, "/build"),
-    filename: "build.js",
-  },
   module: {
     rules: [
-       {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: {
-          transpileOnly: true,
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
         },
-        exclude: /build/,
-      },
-      {
-            test: '/\.(js|jsx)$/',
-              loader: "babel-loader",
-
-              exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader"],
       },
     ],
   },
-  devServer: {
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
-      template: "./public/index.html",
-    }),
-    // DefinePlugin allows you to create global constants which can be configured at compile time
-    new DefinePlugin({
-      "process.env": process.env.production || !process.env.development,
-    })
-  ],
-});
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "build"),
+    compress: true,
+    port: 4000,
+  }
+};
 
-export default webpackConfig;
+export default config;
