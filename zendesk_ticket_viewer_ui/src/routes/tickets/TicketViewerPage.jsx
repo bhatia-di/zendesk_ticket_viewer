f3a6b2dcd3378d37e0fee3a4c77c02b393aa5c99
bhatia-di <bhatia.di@northeastern.edu>
2021-11-26 20:38:34 -0500

    connecion to front end and back end

--- zendesk_ticket_viewer_ui/src/routes/tickets/TicketViewerPage.jsx
+++ zendesk_ticket_viewer_ui/src/routes/tickets/TicketViewerPage.jsx
@@ -1,23 +1,48 @@
 import React, { useState } from "react";
 import ReactLoading from 'react-loading';
 import * as APIURLS from "../../constants/APIConstants";
+import Accordion from 'react-bootstrap/Accordion';
+
 
 export default function TicketViewerPage() {
   const [tickets, setTickets] = useState(null);
   React.useEffect(() => {
         fetch(APIURLS.getTicketsURL).then(response => response.json())
         .then(result => {
-            console.log(result);
+            setTickets(result.tickets);
         });
-
-
-  }, []);
+ }, []);
 
 
   if (tickets) {
     return (
     <div className={"container-fluid"}>
-      <h1><i class="fa fa-amazon"  />You are in TicketViewerPage Section</h1>
+      <h1><i className="fa fa-amazon mr-2"  />You are in TicketViewerPage Section</h1>
+      <Accordion>
+
+        {
+        tickets.map((ticket, index) =>
+
+          <Accordion.Item key={"accordion" + index + ""}>
+              <Accordion.Header>{ticket.subject}</Accordion.Header>
+              <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+              </Accordion.Body>
+
+          </Accordion.Item>
+
+
+
+
+        )
+        }
+    </Accordion>
     </div>
 
 
